1b9b7308b275d8ac2cf1d2aec99172d2988673f4
eMaM1921990 <emam151987@gmail.com>
2022-06-28 18:58:27 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,26 +12,20 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     //of(1, 2, 3, 2, 4).subscribe((d) => console.log(d));
-    from([1,2,3,2,2]).subscribe((d) => console.log(d));
-    
+    //from([1, 2, 3, 2, 2]).subscribe((d) => console.log(d));
+    const subject = new BehaviorSubject(0); // 0 is the initial value
+
+    subject.subscribe({
+      next: (v) => console.log(`observerA: ${v}`),
+    });
+
+    subject.next(1);
+    subject.next(2);
+
+    subject.subscribe({
+      next: (v) => console.log(`observerB: ${v}`),
+    });
+
+    subject.next(3);
   }
-
-
-
-
-  const subject = new BehaviorSubject(0); // 0 is the initial value
- 
-  subject.subscribe({
-    next: (v) => console.log(`observerA: ${v}`),
-  });
-  
-  subject.next(1);
-  subject.next(2);
-  
-  subject.subscribe({
-    next: (v) => console.log(`observerB: ${v}`),
-  });
-  
-  subject.next(3);
-
 }
